32ae7ebfa379861710b897104344f452ad1fbe4f
vara-prasad-789 <vemulavaraprasad709@gmail.com>
2023-01-06 12:48:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo">
+        <div className="onelogoone">
+          <h1>HTML & CSS</h1>
+          <p>design and buid websites</p>
+        </div>
+        <p>John Duckett</p>
+      </div>
     </div>
   );
 }
